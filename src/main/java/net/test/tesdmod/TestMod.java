aa42106a58803d3aa74c4e8a90e772b242876d03
MyDeskTopPC <kimutomo@smkwlab.net>
2020-02-16 16:32:06 +0900

    Update Files

---
+++ src/main/java/net/test/tesdmod/TestMod.java
@@ -0,0 +1,10 @@
+package net.test.tesdmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = TestMod.MODID, name = TestMod.NAME, version = TestMod.VERSION)
+public class TestMod {
+	public static final String MODID = "testmod";
+	public static final String NAME = "Test MOD";
+	public static final String VERSION = "1.0";
+}
